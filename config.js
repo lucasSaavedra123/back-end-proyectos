2126a246423525e36d0b3edf0a3f485136319730
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2022-06-18 18:53:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  NODE_ENV: "mongodb+srv://projectsPSA:<password>@clusterpsa.yp2pamo.mongodb.net/?retryWrites=true&w=majority"
+  NODE_ENV: "mongodb+srv://projectsPSA:projectpsa@clusterpsa.yp2pamo.mongodb.net/?retryWrites=true&w=majority"
 }
